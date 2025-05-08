cbfb9f5c09e44f7137cb98d3be47aa9875a124f5
songkaiang <songkaiang@126.com>
2025-05-08 18:00:07 +0800

    添加MES模块中的工艺管理-基础工艺

--- ruoyi-mes/src/main/java/com/ruoyi/warehouse/controller/MaterialWarehouseInventoryController.java
+++ ruoyi-mes/src/main/java/com/ruoyi/warehouse/controller/MaterialWarehouseInventoryController.java
@@ -8,10 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
@@ -20,7 +17,6 @@ import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.warehouse.domain.MaterialWarehouseInventory;
 import com.ruoyi.warehouse.service.IMaterialWarehouseInventoryService;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
 
 /**
@@ -71,36 +67,4 @@ public class MaterialWarehouseInventoryController extends BaseController
         return success(materialWarehouseInventoryService.selectMaterialWarehouseInventoryById(id));
     }
 
-    /**
-     * 新增物料实时库存
-     */
-    @PreAuthorize("@ss.hasPermi('warehouse:inventory:add')")
-    @Log(title = "物料实时库存", businessType = BusinessType.INSERT)
-    @PostMapping
-    public AjaxResult add(@RequestBody MaterialWarehouseInventory materialWarehouseInventory)
-    {
-        return toAjax(materialWarehouseInventoryService.insertMaterialWarehouseInventory(materialWarehouseInventory));
-    }
-
-    /**
-     * 修改物料实时库存
-     */
-    @PreAuthorize("@ss.hasPermi('warehouse:inventory:edit')")
-    @Log(title = "物料实时库存", businessType = BusinessType.UPDATE)
-    @PutMapping
-    public AjaxResult edit(@RequestBody MaterialWarehouseInventory materialWarehouseInventory)
-    {
-        return toAjax(materialWarehouseInventoryService.updateMaterialWarehouseInventory(materialWarehouseInventory));
-    }
-
-    /**
-     * 删除物料实时库存
-     */
-    @PreAuthorize("@ss.hasPermi('warehouse:inventory:remove')")
-    @Log(title = "物料实时库存", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{ids}")
-    public AjaxResult remove(@PathVariable Long[] ids)
-    {
-        return toAjax(materialWarehouseInventoryService.deleteMaterialWarehouseInventoryByIds(ids));
-    }
 }
